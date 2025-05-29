8d2bb5e87713d9f50bc0dcdc870176a192bac5d4
bernardomonclaro <usuarioususario@MacBook-Pro-de-usuario.local>
2025-05-29 14:34:28 -0300

    push to github

--- index.js
+++ index.js
@@ -20,9 +20,18 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get("/api/whoami", function (req, res) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: software
+  });
+}
+);
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
